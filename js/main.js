32c31b2962e4c98346d7d00d4730625395355efc
Владислав Ким <68331578+vladislav-kim-web@users.noreply.github.com>
2020-07-15 15:12:36 +0300

    загрузка

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+
+    $('.slider-inner').slick({
+        nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false
+
+    })
+
+    $('select').styler()
+
+    $('.header__btn-menu').on('click', function () {
+        $('.menu ul').slideToggle()
+    })
+
+})
